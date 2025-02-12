a43b5d798702e325136da16b4cc4dbd950043aac
Cainan <cainanrhoden@gmail.com>
2025-02-12 12:04:55 -0300

    Add Models Customer, Product, Order, ShippingAddress,

--- store/models.py
+++ store/models.py
@@ -1,3 +1,63 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 # Create your models here.
+
+class Customer(models.Model):
+  user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+  name = models.CharField(max_length=200, null=True)
+  email = models.EmailField(max_length=200)
+  cpf = models.CharField(
+        max_length=11,
+        validators=[RegexValidator(regex='^\d{11}$', message='O CPF deve ter 11 dígitos, apenas números.')],
+        unique=True,
+        null=True,
+        blank=True
+    )
+
+  def __str__(self):
+    return self.name
+
+class Product(models.Model):
+  name = models.CharField(max_length=200)
+  price = models.FloatField()
+  digital = models.BooleanField(default=False, null=True, blank=True)
+  # image = models.ImageField(null=True, blank=True)
+
+  def __str__(self):
+    
+    return self.name
+  
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True, blank=False)
+    transaction_id = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+      return str(self.id)
+    
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+class ShippingAddress(models.Model):
+  customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+  order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+  address = models.CharField(max_length=200, null=True)
+  city = models.CharField(max_length=200, null=True)
+  state = models.CharField(max_length=200, null=True)
+  zipcode = models.CharField(max_length=200, null=True)
+  date_added = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return self.address
+
+
+  # @property
+  # def shipping(self):
+  #   shipping = False
+  #   orderitems = self.orderitem_set.all()
