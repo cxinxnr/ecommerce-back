4fe30d5351ffa67bbe55d351dab4a7ab8cf092f3
Cainan <cainanrhoden@gmail.com>
2025-02-12 14:07:29 -0300

    add dynamic cart data structure

--- store/views.py
+++ store/views.py
@@ -8,8 +8,19 @@ def store(request):
     return render(request, 'store/store.html', context)
 
 def cart(request):
-    context = {}
-    return render(request, 'store/cart.html')
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        # cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+        # cartItems = order['get_cart_items']
+    
+    context = {'items':items, 'order':order}
+    return render(request, 'store/cart.html',context)
 
 
 def checkout(request):
