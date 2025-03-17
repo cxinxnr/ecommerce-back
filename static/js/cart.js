59fedd76d4254e9b831123ce8998995b88cb7b0f
Cainan <cainanrhoden@gmail.com>
2025-03-17 12:04:16 -0300

    Create:cart/cookies, add and remove btns, build order

--- static/js/cart.js
+++ static/js/cart.js
@@ -8,13 +8,37 @@ for (i = 0; i < updateBtns.length; i++) {
 
     console.log('USER:', user)
     if (user == 'AnonymousUser'){
-      console.log('User is not authenticated')
-      // addCookieItem(productId, action)
+      // console.log('User is not authenticated')
+      addCookieItem(productId, action)
     }else{
       console.log('User is authenticated')
       updateUserOrder(productId, action)
     }
   })
+}
+
+function addCookieItem(productId, action){
+  console.log('User not auth')
+
+  if (action == 'add'){
+    if (cart[productId] == undefined){
+      cart[productId] = {'quantity':1}
+    }else{
+      cart[productId]['quantity'] += 1
+    }
+  }
+
+  if (action == 'remove'){
+    if (cart[productId]['quantity'] > 1){ 
+      cart[productId]['quantity'] -= 1
+    }else{
+      delete cart[productId]
+    }
+  }
+
+  console.log('CART:', cart)
+  document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
+  location.reload()
 }
 
 function updateUserOrder(productId, action){
